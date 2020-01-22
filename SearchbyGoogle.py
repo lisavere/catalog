a19396dd51d5cac6c5a4c4de4bf902efa2ce5968
lisavere <verepekinaelizabeth@yandex.ru>
2020-01-23 00:46:47 +0700

    SearchBygoogle and yandex added

---
+++ SearchbyGoogle.py
@@ -0,0 +1,19 @@
+﻿import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://google.ru/")
+time.sleep(5)
+
+textinput = driver.find_element_by_css_selector(".gLFyf")
+
+textinput.send_keys("АГУ")
+time.sleep(3)
+ 
+submit_button = driver.find_element_by_css_selector(".gNO89b")
+
+submit_button.click()
+time.sleep(10)
+
+driver.quit()
---
+++ SearchbyYandex.py
@@ -0,0 +1,19 @@
+﻿import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://yandex.ru/")
+time.sleep(5)
+
+textinput = driver.find_element_by_css_selector(".input__input")
+
+textinput.send_keys("Алтайский государственный университет")
+time.sleep(5)
+
+submit_button = driver.find_element_by_css_selector(".suggest2-form__button")
+
+submit_button.click()
+time.sleep(15)
+
+driver.quit()
